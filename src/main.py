781b7d8a9f7a63072db317c0c9e1738827fae4f3
Aleksandr Simonov <asimonov6317@gmail.com>
2025-12-09 22:17:46 +0200

    fix: makes manual control work

--- src/main.py
+++ src/main.py
@@ -9,25 +9,18 @@
 
 # Library imports
 from vex import *
-import math
-from enum import Enum
-
-class Mode(Enum):
-    DISABLED = 0
-    MANUAL = 1
-    AUTO = 2
 
 brain = Brain()
 controller = Controller()
 
 sensitivity = 1
-mode = Mode.DISABLED
+mode = 0
 
 # Connection scheme
-motorLF = Motor(Ports.PORT1)
-motorLR = Motor(Ports.PORT2)
-motorRF = Motor(Ports.PORT3)
-motorRR = Motor(Ports.PORT4)
+motorLF = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)
+motorRF = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
+motorLR = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+motorRR = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
 motorIntake = Motor(Ports.PORT11)
 
 
@@ -37,28 +30,21 @@ def manual_control():
     
     # See https://www.vexforum.com/t/wiki/67132/33
     # Or https://www.youtube.com/watch?v=gnSW2QpkGXQ
-    y = controller.axis3.position()
-    x = controller.axis4.position()
+    forward = controller.axis3.position()
+    sideways = controller.axis4.position()
     turn = controller.axis1.position()
 
     # don't touch
-    theta = math.atan2(y, x)
-    power = math.hypot(x, y)
+    
+    leftB = forward - sideways - turn
+    rightB = forward + sideways - turn
+    leftF = forward + sideways + turn
+    rightF = forward - sideways + turn
 
-    sin = math.sin(theta - math.pi/4)
-    cos = math.cos(theta - math.pi/4)
-
-
-    velocity_LF = sensitivity * (power * cos + turn)
-    velocity_RF = sensitivity * (power * sin + turn)
-
-    velocity_LR = sensitivity * (power * sin - turn)
-    velocity_RR = sensitivity * (power * cos - turn)
-
-    motorLF.spin(FORWARD, max(-100, min(velocity_LF, 100)), PERCENT)
-    motorLR.spin(FORWARD, max(-100, min(velocity_LR, 100)), PERCENT)
-    motorRR.spin(FORWARD, max(-100, min(velocity_RR, 100)), PERCENT)
-    motorRF.spin(FORWARD, max(-100, min(velocity_RF, 100)), PERCENT)
+    motorLF.spin(FORWARD, leftF, PERCENT)
+    motorRF.spin(FORWARD, rightF, PERCENT)
+    motorLR.spin(FORWARD, leftB, PERCENT)
+    motorRR.spin(FORWARD, rightB, PERCENT)
 
 
     if controller.buttonL1.pressing():
@@ -91,31 +77,28 @@ def draw(display):
     display.clear_screen()
     display.set_cursor(1, 1)
 
-    display.print("Mode: " + mode.name)
+    display.print("Mode: " + str(mode))
     display.next_row()
     display.print("Sens: " + str(sensitivity))
     display.next_row()
     display.print("L")
-    display.set_cursor(display.row(), 19)
+    display.set_cursor(display.row(), 24)
     display.print("R")
 
 
 while True:
-
     if controller.buttonRight.pressing():
-        if mode.value != 2:
-            mode = Mode(mode.value + 1)
+        if mode != 2:
+            mode = mode + 1
     elif controller.buttonLeft.pressing():
-        if mode.value != 0:
-            mode = Mode(mode.value - 1)
+        if mode != 0:
+            mode = mode - 1
 
-
-    if mode.value == Mode.DISABLED:
+    if mode == 0:
         disable()
-    elif mode.value == Mode.MANUAL:
+    elif mode == 1:
         manual_control()
-    elif mode.value == Mode.AUTO:
+    elif mode == 2:
         auto_control()
 
-    draw(controller.screen)
-    
+    draw(controller.screen)    
